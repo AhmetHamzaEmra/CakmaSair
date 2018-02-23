9255097dab39f3d3868ee447b625d31f2a9f0147
BedirT <mtapkan@na.edu>
2018-02-23 07:45:05 -0600

    data added

--- scraper_antoloji.py
+++ scraper_antoloji.py
@@ -27,13 +27,20 @@ def getPoetsPage(base_link):
 			links.append('https://www.antoloji.com' + poets.get('href'))
 		else:
 			odd = True
-	print (base_link[-2])
-	return links, base_link[-2]
+	if str.isdigit(base_link[-3:-1]):
+		print ("here ", base_link[-3:-1])
+		return links, base_link[-3:-1]
+	else:
+		print (base_link[-2])
+		return links, base_link[-2]
 
 ###########################SecondPage###########################
 
-def getTheLinks(base_link):
-	page = requests.get(base_link)
+def getTheLinks(base_link, main_page_num):
+	if main_page_num == 1:
+		page = requests.get(base_link)
+	else:
+		page = requests.get(base_link + "ara-/sirala-/sayfa-" + str(main_page_num) + "/")
 	html_doc = page.text
 	soup = BeautifulSoup(html_doc, 'html.parser')
 
@@ -41,6 +48,7 @@ def getTheLinks(base_link):
 	for div in soup.find_all("div", {'class':'list-number'}):
 		links_of_poems.append("https://www.antoloji.com" + div.find('a').get('href'))
 		# print("https://www.antoloji.com" + div.find('a').get('href'))
+		print("." , end="")
 	return links_of_poems
 
 ############################ThirdPage#############################
@@ -56,8 +64,9 @@ def getThePoem(base_link, pageId):
 			# print(para.get_text().strip())
 			properString = correctTheString(para.get_text())
 			poem += properString
+
 	poem += "<end>\n"
-	with open('siirler_page'+ str(pageId) +'.txt', 'a') as the_file:
+	with open('Data/siirler_page'+ str(pageId) +'.txt', 'a') as the_file:
 		the_file.write(poem + "\n")
 
 ########################## RUNNER ##################################
@@ -71,22 +80,23 @@ def correctTheString(s):
 def processCrawl(page_link):
 	links, page_id = getPoetsPage(page_link)
 	for link_of_poet in links:
-		poem_links = getTheLinks(link_of_poet)
-		for link in poem_links:
-			getThePoem(link, page_id)
+		for i in range(7, 10):
+			poem_links = getTheLinks(link_of_poet, i)
+			for link in poem_links:
+				getThePoem(link, page_id)
 
 def __main__():
 
 	# upper bound for pages is 18
 	start_page = int(input("Start From Page number : "))
 	how_many_pages = int(input("Crawl this many pages : "))
+	# poems_page_count = int(input("How many pages do you want to crawl from each poet : "))
 	pages = [] #['https://www.antoloji.com/populer-sairler/']
-	for i in range(start_page, start_page + how_many_pages + 1):
+	for i in range(start_page, start_page + how_many_pages):
 		pages.append("https://www.antoloji.com/populer-sairler/sirala-/sayfa-" + str(i) + "/")
 
 	num_cores = multiprocessing.cpu_count()
 
 	Parallel(n_jobs=num_cores)(delayed(processCrawl)(i) for i in pages)
-
 
 __main__()
