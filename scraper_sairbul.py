118c6f4b95d0ad1d49026311f00cc332f6d4b424
BedirT <mtapkan@na.edu>
2018-02-24 19:59:21 -0600

    new data

--- scraper_sairbul.py
+++ scraper_sairbul.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from joblib import Parallel, delayed
 import multiprocessing
+import re
 
 def getPoetsPage(base_link):
 	page = requests.get(base_link)
@@ -16,7 +17,8 @@ def getPoetsPage(base_link):
 	for poets in poet_names_list:
 		names = poets.contents[0]
 		# print(poets.get('href'))
-		links.append([poets.get('href'), poets.get('title')])
+		unicode_string = poets.get('title').encode('latin1').decode('utf8')
+		links.append([poets.get('href'), unicode_string])
 	return links
 
 def getPoems_from_poets(base_link):
@@ -25,11 +27,13 @@ def getPoems_from_poets(base_link):
 	soup = BeautifulSoup(html_doc, 'html.parser')
 
 	poem_names = soup.find(class_ = 'r')
+	if poem_names == None:
+		return []
 
 	poem_list = []
 	if poem_names == None:
 		return poem_list
-	
+
 	for link in poem_names.find_all('h3'):
 	    poem_list.append(link.find('a'))
 
@@ -46,12 +50,36 @@ def get_poem_from_page(base_link):
 	soup = BeautifulSoup(html_doc, 'html.parser')
 
 	poem_names = soup.find(class_ = 'present')
+	if poem_names == None:
+		print("Error parsing no class found: " + base_link)
+		return ""
 
+	poem = _with_tag_p(poem_names)
+	if poem == "<start>\n":
+		poem = _with_class(poem_names)
+		if poem == "<start>\n":
+			poem = _with_tag_span(poem_names)
+			if poem == "<start>\n":
+				print("Error parsing no paragraph link found: " + base_link)
+				return ""
+	poem += "\n<end>\n"
+	# print(poem)
+	return poem
+
+def _with_class(poem_names):
+	poem = "<start>\n"
+	for para in poem_names.find_all(class_ = 'p pt5'):
+		poem += (para.get_text("\n").encode('latin1', 'ignore').decode('utf8', "ignore"))
+	return poem
+def _with_tag_p(poem_names):
 	poem = "<start>\n"
 	for para in poem_names.find_all('p'):
-		print(para.get_text().strip())
-		poem += para.get_text()
-	poem += "<end>\n"
+		poem += (para.get_text("\n").encode('latin1', 'ignore').decode('utf8', "ignore"))
+	return poem
+def _with_tag_span(poem_names):
+	poem = "<start>\n"
+	for para in poem_names.find_all('span'):
+		poem += (para.get_text("\n").encode('latin1', 'ignore').decode('utf8', "ignore"))
 	return poem
 
 ########################## RUNNER ##################################
@@ -59,10 +87,10 @@ def get_poem_from_page(base_link):
 def processCrawl(link):
 	poem_list = []
 	for page_num in range(1, 10):
-		# print(str(link[0][:-12]) + str(page_num) + ".sairbul.com")
-		poem_list = getPoems_from_poets(str(link[0][:-12]) + str(page_num) + ".sairbul.com")
-		if(poem_list == None):
-			exit()
+		poem_list += getPoems_from_poets(str(link[0][:-12]) + str(page_num) + ".sairbul.com")
+		# if len() > 0:
+		# 	print(str(link[0][:-12]) + str(page_num) + ".sairbul.com")
+
 	for poem in poem_list:
 		with open('Data2/'+ link[1] +'.txt', 'a') as the_file:
 			the_file.write(get_poem_from_page(poem) + "\n")
@@ -72,7 +100,9 @@ def __main__():
 
 	num_cores = multiprocessing.cpu_count()
 	l = getPoetsPage("http://www.sairbul.com")
+	# l = ['http://abdurrahim-karakoc.sairbul.com/acaba/', 'http://ahmet-hasim.sairbul.com/bir-yaz-gecesi-hatirasi/', 'http://ahmet-hasim.sairbul.com/agac/']
+	Parallel(n_jobs=num_cores)(delayed(processCrawl)(i) for i in l[42:])
 
-	Parallel(n_jobs=num_cores)(delayed(processCrawl)(i) for i in l)
+	# print(get_poem_from_page("http://ahmet-hasim.sairbul.com/agac/"))
 
 __main__()
